83ed9bdc9c69fa9fb083e29fcc42acad7ad70b9e
ajb007 <andrew.bartlett@digital.landregistry.gov.uk>
2016-03-11 21:06:11 +0000

    Basic API framework working

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.orm.attributes import flag_modified
 import os
 
 
@@ -11,13 +12,13 @@ from models import *
 
 @app.route('/create', methods=['POST'])
 def create_player():
-    print('Here')
-    if not request.json or not 'player' in request.json:
+    # curl -i -H "Content-Type: application/json" -X POST -d '{"name":"Andy"}' http://localhost:5000/create
+    if not request.json or not 'name' in request.json:
         abort(400)
     player = {
-        'name': request.json['player'],
+        'name': request.json['name'],
         'xpos': 0,
-        'ypos': 0,
+        'ypos': 0
     }
     try:
         result = Player(
@@ -30,12 +31,33 @@ def create_player():
     return jsonify({'player': player}), 201
 
 @app.route('/player/<int:id>', methods=['GET'])
-def get_task(id):
+def get_player(id):
     player = Player.query.get(id).player
     print(player)
     #if player == None:
 #        abort(404)
     return jsonify({'player': player}), 201
+
+@app.route('/update/<int:id>', methods=['PUT'])
+def update_player(id):
+
+    # curl -i -H "Content-Type: application/json" -X PUT -d '{"name":"Drew"}' http://localhost:5000/update/10
+    # Need to validate the request
+    print("**************")
+    print(request.get_json())
+    updated_player = request.get_json()
+    print("**************")
+    try:
+        player = Player.query.get(id)
+        player.player = {
+            'name': updated_player['name'],
+            'xpos': updated_player['xpos'],
+            'ypos': updated_player['ypos']
+        }
+        db.session.commit()
+    except Exception as e:
+        raise
+    return jsonify({'player': player.player}), 201
 
 @app.route('/')
 def hello():
